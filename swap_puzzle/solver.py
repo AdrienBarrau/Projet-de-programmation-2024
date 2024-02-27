98e267cef02a6488d46d94a7993d6733dde7153f
adbarrau-ensae <adrien_barrau@ensae.fr>
2024-02-27 16:12:43 +0000

    solver.py

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -6,8 +6,11 @@ class Solver(): # chemin_grille est une liste de tuples de tuples contenant le c
     def __init__(self, chemin_grille=[]):
         self.chemin_grille=chemin_grille
 
-    def get_solution(self):
-        "\n"
+    def affiche_matrices(self):   #affiche le chemin de matrices
+        print(self.chemin_grille)
+
+    def get_solution(self):   #retourne les swaps a effectuer
+        print("\n")
         liste=self.chemin_grille
         res=[]   #liste des swaps
         for k in range(0,len(liste)-1):
@@ -29,8 +32,9 @@ chemin1=Solver(Graph.solve_bfs(grille_ex))
 
 
 print(chemin1.get_solution())  #donne la liste des swaps a effectuer
+chemin1.affiche_matrices()
 
-print(grille_ex.get_solution_naive()) #meme nombre de swaps pour une grille 2 sur 2 : 4 swaps
+#print(grille_ex.get_solution_naive()) #meme nombre de swaps pour une grille 2 sur 2 : 4 swaps
 
 
 
@@ -41,6 +45,8 @@ chemin2=Solver(Graph.solve_bfs(grille_ex2))
 
 
 print(chemin2.get_solution())           #16 swaps pour une grille 3 sur 3
-print(grille_ex2.get_solution_naive())  #18 swaps pour une grille 3 sur 3
+chemin2.affiche_matrices()
+
+#print(grille_ex2.get_solution_naive())  #18 swaps pour une grille 3 sur 3
 
 
