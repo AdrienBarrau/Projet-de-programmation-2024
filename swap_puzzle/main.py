bb7df46306aedb5bf723d5bdc511d1f49c99ff27
dianesaint <156657595+dianesaint@users.noreply.github.com>
2024-03-10 18:16:50 +0100

    Update main.py

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -23,30 +23,30 @@ def game():
     # on détermine forme matrice et solution
     lines = int(input("nombre de lignes: "))
     columns = int(input("Nombre de colonnes: "))
-    destination= Graphics.construction_ex(lines, columns)
     # déterminer le bon lambda pour utiliser A* ( voir fonction Adrien)
     # choix de la difficulté ( facile = peu de swaps, moyen = plus de swaps, dur = + de swap et des barrières)
     level = input("Tapez 'facile', 'moyen' ou 'difficile' :")
     if level == "facile":
         while solution >3:
-        range = randrange(1,lines*columns)    #pour prendre une matrice au hasard il faudrai plutot tirer un nb au hasard entre 0 et (m*n)!-1
-        all = Graph.generate_matrices(lines, columns)
-        for_game = all[range]
+            range = randrange(1,lines*columns)    #pour prendre une matrice au hasard il faudrai plutot tirer un nb au hasard entre 0 et (m*n)!-1
+            all = Graph.generate_matrices(lines, columns)
+            for_game = all[range]
     # déterminer le nb de swaps optimal
-        if lines + columns =< 6:
-            solution = time_and_solve_new_bfs([for_game])[2]
+        if lines + columns <= 6:
+            solution = time_and_solve_new_bfs([for_game])[0][2]
         elif lines + columns > 6
-            solution = time_and_solve_a_star([for_game])[2]
+            solution = time_and_solve_a_star([for_game])[0][2]
     elif level == "moyen":
         while solution <4:
-        range = randrange(1,lines*columns)
-        all = Graph.generate_matrices(lines, columns)
-        for_game = all[range]
-        if lines + columns =< 6:
-            solution = time_and_solve_new_bfs([for_game])[2]
+            range = randrange(1,lines*columns)
+            all = Graph.generate_matrices(lines, columns)
+            for_game = all[range]
+        if lines + columns <= 6:
+            solution = time_and_solve_new_bfs([for_game])[0][2]
         elif lines + columns > 6
-            solution = time_and_solve_a_star([for_game])[2]
+            solution = time_and_solve_a_star([for_game])[0][2]
     elif level == "difficile": #voir selon si on a le temps de faire les barrières
+        print("hahaha")
     print(solution)
     # modéliser la grille avec pygame, avec tous les trucs nécessaires, rajouter l'option réussite et l'options pas réussite
     Graphics.display_pygame(for_game, solution)
